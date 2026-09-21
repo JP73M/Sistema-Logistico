a2ad5b8722ed448933ae8b1de9524cc98e41a9b9
Juan Melo <juanpablomelo73@gmail.com>
2026-09-21 10:29:31 -0500

    primer lanzamiento

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const ventana = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    });
+
+    ventana.loadFile(
+        path.join(__dirname, "Frontend", "pages", "login.html")
+    );
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
